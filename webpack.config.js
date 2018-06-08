14a35504116386ecd380650845196d440d019de8
ranjayzheng(郑仁杰) <ranjayzheng@tencent.com>
2018-06-08 17:43:14 +0800

    '20180608Ranjay'

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
+// 注入HTML
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 提取CSS插件
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// 清除冗余文件插件
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 // webpack4无法自动压缩.css文件，需要下面的插件支持
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
@@ -12,6 +15,7 @@ module.exports = {
   resolve: {
     extensions: ['.jsx', '.js']
   },
+  // webpack4压缩插件配置
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -24,9 +28,11 @@ module.exports = {
   },
   module: {
     rules: [{
+      // css-loader
       test: /\.css$/,
       use: [MiniCssExtractPlugin.loader, 'css-loader']
     }, {
+      // babel-loader  (ES6)
       test: /\.jsx?$/,
       exclude: /node_modules/,
       use: {
@@ -42,6 +48,7 @@ module.exports = {
       // 打包后文件名称，会自动放到 output 指定的 dist 目录
       filename: 'index.html'
     }),
+    // 提取CSS
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
       chunkFilename: "[id].css"
