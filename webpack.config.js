c60fee515a4585a1852633bf583f791d2a6212e8
ranjayzheng(郑仁杰) <ranjayzheng@tencent.com>
2018-06-08 16:08:31 +0800

    add miniCSSExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+// webpack4无法自动压缩.css文件，需要下面的插件支持
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
   entry: './src/index.jsx',
   resolve: {
     extensions: ['.jsx', '.js']
   },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      use: [MiniCssExtractPlugin.loader, 'css-loader']
     }, {
       test: /\.jsx?$/,
       exclude: /node_modules/,
@@ -19,12 +35,21 @@ module.exports = {
     }]
   },
   plugins: [
-        // 加入 html 模板任务
+    // 加入 html 模板任务
     new HtmlWebpackPlugin({
-            // 模板文件
+      // 模板文件
       template: 'assets/index.html',
-            // 打包后文件名称，会自动放到 output 指定的 dist 目录
+      // 打包后文件名称，会自动放到 output 指定的 dist 目录
       filename: 'index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+      chunkFilename: "[id].css"
+    }),
+    new CleanWebpackPlugin('dist/*', {
+      root: __dirname,
+      verbose: true,
+      dry: false
+    }),
   ]
 }
