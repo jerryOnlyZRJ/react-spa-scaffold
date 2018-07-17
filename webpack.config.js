b17ebc3ee37f860a4971279f82052586945f1094
ranjayzheng <ranjayzheng@tencent.com>
2018-07-17 17:01:08 +0800

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'scripts/[name]-[hash:5].js'
+  },
   resolve: {
     extensions: ['.jsx', '.js']
   },
@@ -24,13 +29,37 @@ module.exports = {
         sourceMap: true // set to true if you want JS source maps
       }),
       new OptimizeCSSAssetsPlugin({})
-    ]
+    ],
+    splitChunks: {
+      cacheGroups: {
+        vendor: { // 抽离第三方插件
+          test: /node_modules/, // 指定是node_modules下的第三方包
+          chunks: 'initial',
+          name: 'common/vendor', // 打包后的文件名，任意命名    
+          priority: 10 // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+        },
+        utils: { // 抽离自定义公共代码
+          test: /\.js$/,
+          chunks: 'initial',
+          name: 'common/utils',
+          minSize: 0 // 只要超出0字节就生成一个新包
+        }
+      }
+    }
   },
   module: {
     rules: [{
       // css-loader
       test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader']
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss'
+          }
+        }
+      ]
     }, {
       // babel-loader  (ES6)
       test: /\.jsx?$/,
@@ -50,8 +79,8 @@ module.exports = {
     }),
     // 提取CSS
     new MiniCssExtractPlugin({
-      filename: 'styles/[name].css',
-      chunkFilename: "[id].css"
+      filename: 'styles/[name]-[hash:5].css',
+      chunkFilename: "styles/[id]-[hash:5].css"
     }),
     new CleanWebpackPlugin('dist/*', {
       root: __dirname,
