7bd34d97be5d38573645d998e5800f613e2743c6
ranjayzheng <ranjayzheng@qq.com>
2018-12-20 21:15:45 +0800

    add jest

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/assets/styles/index.css'
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return <h1>Hello React!</h1>
   }
